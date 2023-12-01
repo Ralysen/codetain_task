a9597a4b1e5a93e663ba1907e8c6c48fbb7d7b34
Radoslaw Lyson <radoslawlyson@gmail.com>
2023-12-01 14:25:44 +0100

    Base project configuration

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send("Hello, its me");
+})
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http:localhost:${port}`);
+})
